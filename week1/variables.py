40eef3b102ccb0850f26e13ef21b41729fd6c3bc
Stephen St Jean <stephenstjean1@gmail.com>
2025-12-20 16:53:55 -0500

    Week 1: variables and output

---
+++ week1/variables.py
@@ -0,0 +1,6 @@
+Your_Name = "Stephen"
+Your_Age = 20
+Your_Favorite_Game = "Terraria"
+
+msg = f"My name is {Your_Name}.\nI am {Your_Age} years old.\nMy favorite game is {Your_Favorite_Game}"
+print(msg)
